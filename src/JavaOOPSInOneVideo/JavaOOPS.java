3b52b1584cc57e08d100c8cc96d27f2d186870cd
Rishabh Kumar <rsk1998200@gmail.com>
2024-03-31 03:46:07 +0530

    OOPS polymorphism, constructor

---
+++ src/JavaOOPSInOneVideo/JavaOOPS.java
@@ -0,0 +1,69 @@
+package JavaOOPSInOneVideo;
+
+public class JavaOOPS {
+    // In .java ext file only one [public] class can exist where filename and public class name will be same.
+    public static void oops () {
+        // In real world and programming everything is an Object.
+
+        // Classes and Objects
+        // Class is a blueprint of an object.
+        // Object is an instance of a class.
+
+        // Using default constructor to create an object/instance
+        Person person = new Person();
+        person.name = "Rishabh";
+
+        // Using default constructor to create an object/instance
+        Person person2 = new Person();
+        person2.name = "Vansh";
+
+        person.displayName();
+        person2.displayName();
+        person.eat();
+
+        // Following polymorphism (Compile time)
+        person2.walk();
+        person2.walk(10000);
+
+        System.out.println(Person.count);
+    }
+}
+
+class Person {
+    // Properties
+    String name;
+    int age;
+    // static means property of a class not an object
+    // to access this property we don't need to create any object.
+    static int count; // total number of persons
+
+    // Default constructor (Optional to declare)
+    public Person () {
+        count++;
+        System.out.println("Default constructor called");
+    }
+
+    // Methods
+    void displayName () {
+        System.out.println(this.name);
+    }
+
+    void walk () {
+        System.out.println(this.name + " is walking.");
+    }
+
+    void eat () {
+        System.out.println(this.name + " is eating.");
+    }
+
+    // Function overloading
+    void walk (int steps) {
+        System.out.println(this.name + " walked " + steps + " steps.");
+    }
+
+    // Polymorphism => poly + morphism
+    // Taking multiple forms
+    // There are 2 types compile and run-time polymorphism
+    // As we saw above that our walk func can run differently acc to the args passes into it.
+    // This is polymorphism
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import JavaOOPSInOneVideo.JavaOOPS;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        JavaOOPS.oops();
     }
 }
