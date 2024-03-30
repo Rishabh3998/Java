0b3a23c5f44e8b0afa241c9aa73abcb788be731b
Rishabh Kumar <rsk1998200@gmail.com>
2024-03-31 01:30:32 +0530

    data-types, varaiable

---
+++ src/JavaInOneVideo/IntroToJava1Hr.java
@@ -0,0 +1,25 @@
+package JavaInOneVideo;
+
+public class IntroToJava1Hr {
+    public static void variables () {
+        // Introduction to variables in Java
+        // A container that can store some value according to the declared data-type
+
+        // Data types:
+        // byte, short, int, long
+        // float, double
+        // char, string
+        // boolean
+
+        // Syntax: data-type variable-name = value
+        byte byteValue = 10;
+        short shortValue = 12;
+        int age = 18;
+        long longValue = 37821738712L;
+        float floatValue = 1.1f;
+        double doubleValue = 1.1;
+        boolean isAdult = true;
+        char charValue = 'a';
+        String stringValue = "Rishabh";
+    }
+}
