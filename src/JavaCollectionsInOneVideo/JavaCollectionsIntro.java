dcd980f39e48539edd6196375f6b746ee0c3c720
Rishabh Kumar <rsk1998200@gmail.com>
2024-03-31 15:55:24 +0530

    Collections start

---
+++ src/JavaCollectionsInOneVideo/JavaCollectionsIntro.java
@@ -0,0 +1,37 @@
+package JavaCollectionsInOneVideo;
+
+public class JavaCollectionsIntro {
+    // All the entities exists inside collection framework:
+
+    // Interface
+    // class
+
+    // Collection
+
+    // - List
+    //   - ArrayList
+    //   - LinkedList
+    //   - Stack
+    //   - Vector
+
+    // - Set
+    //   - EnumSet
+    //   - HashSet
+    //   - LinkedHashSet
+    //   - TreeSet
+
+    // - Queue
+    //   - ArrayDequeue
+    //   - LinkedList
+    //   - PriorityQueue
+
+    // - Map
+    //   - HashMap
+    //   - TreeMap
+    //   - EnumMap
+    //   - LinkedHashMap
+    //   - WeakHashMap
+
+    // - Iterator
+    //   - ListIterator
+}
